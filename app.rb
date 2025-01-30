70840978295eb1ddb0dd6da91cb76d44f7338912
Adrián Mugnolo <adrian@mugnolo.com>
2025-01-30 17:02:52 -0300

    Load app.rb as a middleware

--- app.rb
+++ app.rb
@@ -1,73 +1,79 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'json'
-require './app/models/game'
 require 'securerandom'
 
-set :public_folder, 'public'
-set :views, File.join(File.dirname(__FILE__), 'app/views')
-set :erb, :layout => :'layouts/application.html'
+require_relative './app/models/game'
 
-# Enable JSON body parsing
-before do
-  if request.content_type == 'application/json'
-    request.body.rewind
-    @request_payload = JSON.parse(request.body.read)
+class MyApp < Sinatra::Base
+  set :public_folder, 'public'
+  set :views, File.join(File.dirname(__FILE__), 'app/views')
+  set :erb, :layout => :'layouts/application.html'
+
+  # Enable JSON body parsing
+  before do
+    if request.content_type == 'application/json'
+      request.body.rewind
+      @request_payload = JSON.parse(request.body.read)
+    end
   end
-end
 
-get '/' do
-  send_file File.join(settings.public_folder, 'index.html')
-end
-
-post '/api/game' do
-  content_type :json
-  players = @request_payload['players']
-  rounds = @request_payload['rounds']
-
-  game = Game.new(players, rounds)
-  game.start
-
-  redirect "/game/#{game.id}"
-end
-
-get '/game/:id' do
-  @game = Game.find(params[:id])
-  erb :spreadsheet
-end
-
-post '/api/ask_for_tricks' do
-  content_type :json
-  player = @request_payload['player']
-  tricks = @request_payload['tricks'].to_i
-  game = Game.find(@request_payload['game_id'])
-
-  begin
-    game.ask_for_tricks(player, tricks)
-    status 200
-  rescue => e
-    status 400
-    { error: e.message }.to_json
+  get '/' do
+    send_file File.join(settings.public_folder, 'index.html')
   end
-end
 
-post '/api/register_tricks' do
-  content_type :json
-  player = @request_payload['player']
-  tricks = @request_payload['tricks'].to_i
-  game = Game.find(@request_payload['game_id'])
+  post '/api/game' do
+    content_type :json
+    players = @request_payload['players']
+    rounds = @request_payload['rounds']
 
-  begin
-    game.register_tricks(player, tricks)
-    status 200
-  rescue => e
-    status 400
-    { error: e.message }.to_json
+    game = Game.new(players, rounds)
+    game.start
+
+    redirect "/game/#{game.id}"
   end
-end
 
-get '/api/leaderboard' do
-  content_type :json
-  game = Game.find(params[:game_id])
-  puts "Leaderboard: #{game.leaderboard.to_json}"
-  game.leaderboard.to_json
+  get '/game/:id' do
+    @game = Game.find(params[:id])
+    erb :spreadsheet
+  end
+
+  post '/api/ask_for_tricks' do
+    content_type :json
+    player = @request_payload['player']
+    tricks = @request_payload['tricks'].to_i
+    game = Game.find(@request_payload['game_id'])
+
+    begin
+      game.ask_for_tricks(player, tricks)
+      status 200
+    rescue => e
+      status 400
+      { error: e.message }.to_json
+    end
+  end
+
+  post '/api/register_tricks' do
+    content_type :json
+    player = @request_payload['player']
+    tricks = @request_payload['tricks'].to_i
+    game = Game.find(@request_payload['game_id'])
+
+    begin
+      game.register_tricks(player, tricks)
+      status 200
+    rescue => e
+      status 400
+      { error: e.message }.to_json
+    end
+  end
+
+  get '/api/leaderboard' do
+    content_type :json
+    game = Game.find(params[:game_id])
+    puts "Leaderboard: #{game.leaderboard.to_json}"
+    game.leaderboard.to_json
+  end
+
+  # Start the server if this file is executed directly.
+  run! if app_file == $0
 end
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require_relative '../app.rb'
+
 module RbLapodridapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -23,5 +25,8 @@ module RbLapodridapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Use the MyApp middleware to handle API requests.
+    config.middleware.use MyApp
   end
 end
